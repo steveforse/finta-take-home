a3b6f7839420cb35f5c966f2dbc2ca767011d4b0
Steve Forse <steve.forse@gmail.com>
2025-11-01 16:44:08 +0700

    Add pagination support to bank account transaction fetcher

--- app/interactions/fetch_bank_account_transactions.rb
+++ app/interactions/fetch_bank_account_transactions.rb
@@ -1,5 +1,6 @@
 class FetchBankAccountTransactions < ActiveInteraction::Base
-  string :account_id
+  string :bank_account_id
+  integer :page, default: 1
 
   def execute
     JSON.parse(fetch_bank_account_transactions)
@@ -9,8 +10,8 @@ class FetchBankAccountTransactions < ActiveInteraction::Base
 
   def fetch_bank_account_transactions
     Faraday.get(
-      "https://take-home-11tn.onrender.com/bank_accounts/#{account_id}/transactions",
-      nil,
+      "https://take-home-11tn.onrender.com/bank_accounts/#{bank_account_id}/transactions",
+      { page: page },
       { "Authorization" => "finta-take-home" }
     ).body
   end
