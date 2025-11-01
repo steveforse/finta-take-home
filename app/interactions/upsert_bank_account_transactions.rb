b03a27a536722b90bda96436f209e80d16b17516
Steve Forse <steve.forse@gmail.com>
2025-11-01 17:07:42 +0700

    Add paging version of bank account transaction upsert

---
+++ app/interactions/upsert_bank_account_transactions.rb
@@ -0,0 +1,39 @@
+class UpsertBankAccountTransactions < ActiveInteraction::Base
+  string :bank_account_id
+
+  def execute
+    upsert_bank_account_transactions
+  end
+
+  private
+
+  def cursor
+    @cursor ||= RemoteBankAccountTransactions.new(bank_account_id: bank_account_id)
+  end
+
+  def upsert_bank_account_transactions
+    transactions = cursor.transactions
+    upsert_batch(transactions)
+
+    while(cursor.has_next?)
+      upsert_batch(cursor.next_page)
+    end
+  end
+
+  def upsert_batch(transactions)
+    transactions.each do |transaction|
+      Transaction
+        .find_or_initialize_by(uuid: transaction["uuid"])
+        .update(
+          date: Time.at(transaction["date"]),
+          amount_cents: transaction["amount_cents"],
+          amount_currency: transaction["amount_currency"],
+          description: transaction["description"],
+          status: transaction["status"],
+          account_type: "BankAccount",
+          account_id: BankAccount.find_by(uuid: transaction["bank_account_id"]).id,
+          remote_data: transaction,
+        )
+    end
+  end
+end
