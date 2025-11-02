901ef7114e2599300228547b6b38bca38c30eb7c
Steve Forse <steve.forse@gmail.com>
2025-11-02 18:41:28 +0700

    Update transactions -> account_transactions

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :category_group
-  has_many :transactions, dependent: :nullify
+  has_many :account_transactions, dependent: :nullify
 
   validates :name, presence: true
   validates :name, uniqueness: { scope: :category_group_id, message: "already exists in this group" }
