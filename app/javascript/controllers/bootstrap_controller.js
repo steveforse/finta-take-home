0da90632232202ab05fd897d520d4bbe1e6d4a77
Steve Forse <steve.forse@gmail.com>
2025-11-02 18:03:25 +0700

    Add Bootstrap to the project

---
+++ app/javascript/controllers/bootstrap_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+import * as bootstrap from "bootstrap"
+
+// Connects Bootstrap components to Stimulus
+export default class extends Controller {
+  connect() {
+    // Initialize tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    
+    // Initialize popovers
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+  }
+}
